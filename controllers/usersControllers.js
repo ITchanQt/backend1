6975ba6f6292390b0eb9df26fbc96df5a503f2a2
ITchanQt <christianmacorol2002@gmail.com>
2025-05-30 05:59:09 -0700

    add create user function

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -16,6 +16,61 @@ const usersControllers = {
             });
         }
     },
+
+    createUser: async (req, res) => {
+        try {
+            const { name, address, phoneNum} = req.body;
+            if (!name || !address || !phoneNum) {
+                return res.status(400).json({
+                    success: false,
+                    message: 'All fields are required'
+                });
+            }
+
+            const userData = {
+                user_name: name,
+                user_add: address,
+                user_num: phoneNum
+            };
+
+           const results = await Users.create(userData);
+           res.status(201).json({
+            success: true,
+            message: 'User created successfuly',
+            data: {
+                id: results.insertId,
+                ...userData
+            }
+           });
+        } catch (error) {
+            res.status(500).json({
+                success: false,
+                message: 'Erorr creating user',
+                error: error.message
+            });
+        }
+    },
+
+    // createUser: async (req, res) => {
+    //     try {
+    //         const userData = req.body;
+    //         const results = await Users.create(userData);
+    //         res.status(201).json({
+    //             success: true,
+    //             message: 'User created successfully',
+    //             data: {
+    //                 id: results.insertID,
+    //                 ...userData
+    //             }
+    //         });
+    //     } catch (error) {
+    //         res.status(500).json({
+    //             success: false,
+    //             message: 'Error creating user',
+    //             error: error.message
+    //         });
+    //     }
+    // }
 };
 
 module.exports = usersControllers;
